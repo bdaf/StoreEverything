9c09307a9875e31f401d32c12e9827924e6eaa66
Kamil Korenkiewicz <kamil@Kamils-MacBook-Air.local>
2022-05-30 20:45:25 +0200

    Add getCurrentUser() method but not tested

--- ProjectJavaWeb/src/main/java/pl/team/marking/projectjavaweb/entity/MyUserDetails.java
+++ ProjectJavaWeb/src/main/java/pl/team/marking/projectjavaweb/entity/MyUserDetails.java
@@ -1,5 +1,7 @@
 package pl.team.marking.projectjavaweb.entity;
 
+
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 public class MyUserDetails implements UserDetails {
     private String login;
     private String name;
--- ProjectJavaWeb/src/main/java/pl/team/marking/projectjavaweb/service/MyUserDetailsServiceImpl.java
+++ ProjectJavaWeb/src/main/java/pl/team/marking/projectjavaweb/service/MyUserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package pl.team.marking.projectjavaweb.service;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,15 @@ public class MyUserDetailsServiceImpl implements MyUserDetailsService {
         Optional<UserApp> user = userRepository.findUserByLogin(aUsername);
         user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + aUsername));
         return user.map(MyUserDetails::new).get();
+    }
+
+    @Override
+    public MyUserDetails getCurrentUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof MyUserDetails) {
+            return ((MyUserDetails)principal);
+        }
+        return null;
     }
 
     @Override
